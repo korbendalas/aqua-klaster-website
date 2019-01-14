43cbcc59924fa2e046606b182ef9ca817a3369ea
Dalibor Petric <daliborpetric99@gmail.com>
2019-01-14 23:21:54 +0100

    Add files via upload
    Aqua Klaster website

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+$('.show-menu-btn').click(function(){
+    $('.navigation__menu').toggleClass('prikazi-nav');
+    $('.show-menu-btn').toggleClass('aktivno');
+    $('.navigation').toggleClass('navigation-active');
+    $('.header-info ').toggleClass('remove-header-info ');
+
+})
+
+})
+
